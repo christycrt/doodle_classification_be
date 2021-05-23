7891cef8e7c92bb28075b5183d39971be73dcb21
christycrt <christycrt@gmail.com>
2021-05-23 22:21:53 +0700

    add ip backend

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def predictDoodleClass(image):
     return {"accuracy": str(predict_class_percent), "predict_class":doodle_classes[predict_class]}, results
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
